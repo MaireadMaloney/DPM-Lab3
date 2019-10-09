11b2d658835f0dad97613c90e95b5fb12cf263c5
jbelanger25 <jonathan.belanger2@mail.mcgill.ca>
2019-10-08 22:15:37 -0400

    Update Main.java

--- src/ca/mcgill/ecse211/lab4/Main.java
+++ src/ca/mcgill/ecse211/lab4/Main.java
@@ -73,7 +73,7 @@ public class Main {
       
       Button.waitForAnyPress();
       
-      LightLocalizer2 lsl = new LightLocalizer2(odo, navigator, colorSensor);
+      LightLocalizer2 lsl = new LightLocalizer2();
       Thread lslThread = new Thread(lsl);
       lslThread.start();
       
